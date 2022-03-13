b2984b27914021a3a4f2c81c4575f9ca83b6e14e
jessicarydberg <jessica.rydberg@hotmail.se>
2022-03-13 15:42:20 +0000

    Add ebasic structure by naming functions and global variable.

--- run.py
+++ run.py
@@ -1,1 +1,36 @@
 import random
+
+HANGMAN =
+
+
+"""
+Creates a list of words and randomly pick and return one of them to play with.
+"""
+def get_word():
+
+
+"""
+Lets the user make a guess and check if it is valid.
+"""
+def get_guess():
+
+
+"""
+Checks if the guess was correct or not.
+"""
+def validate_guess():
+
+
+"""
+Calculate how many tries wich decides wich "image" of the hangman should be displayed.
+"""
+def collect_tries():
+
+
+"""
+Congratulates when game is completed and asks if the user wants to play again.
+"""
+def win_game():
+
+
+def main():
