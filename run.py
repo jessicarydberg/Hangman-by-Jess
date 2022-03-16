b7731aef696b83c92756b2da9c1be2b632caf447
jessicarydberg <jessica.rydberg@hotmail.se>
2022-03-16 09:17:15 +0000

    Adjust word and guess to uppercase to solve bug

--- run.py
+++ run.py
@@ -98,7 +98,7 @@ def get_word():
     elif theme == "furniture":
         words = "table chair sofa bathtub wardrobe bed cabinet".split(" ")
     number = random.randint(0, len(words)-1)
-    word = list(words[number])
+    word = list(words[number].upper())
 
     print("\n")
     print("Can you find out which word that is hidden here?")
@@ -118,31 +118,28 @@ def get_guess(word, hidden_word, tries):
     until it is valid.
     Then checks if the guess is correct or not.
     """
-    print(HANGMAN[len(tries)])
-    print((' '.join(hidden_word)).upper())
     print("\n")
     if len(tries) > 0:
         print("Failed guesses: " + (' '.join(tries)).upper() + "\n")
     while True:
-        guess = input("Please make a guess: \n")
+        guess = input("Please make a guess: \n").upper()
         print("\n")
-        if validate_guess(guess, word, hidden_word, tries):
+        list_guess = list(guess)
+        if validate_guess(list_guess, guess, word, hidden_word, tries):
             break
-    guess = list(guess)
-    if guess == word:
-        guess = ''.join(guess).upper()
+    if list_guess == word:
         print("\n")
-        print(f'Good guess! {guess} is the word')
+        print(f'Good guess! {guess.upper()} is the word')
         end_game("win")
-    elif len(guess) > 1 and guess != word:
+    elif len(list_guess) > 1 and list_guess != word:
         collect_tries(tries, word, hidden_word, guess)
-    elif guess[0] in word:
+    elif guess in word:
         add_letter(guess, word, hidden_word, tries)
     else:
         collect_tries(tries, word, hidden_word, guess)
 
 
-def validate_guess(guess, word, hidden_word, tries):
+def validate_guess(list_guess, guess, word, hidden_word, tries):
     """
     Validates the data provided in the input for guessing.
     Prints error messages if the data contains other symbols then letters.
@@ -151,19 +148,19 @@ def validate_guess(guess, word, hidden_word, tries):
     """
     try:
         if guess.isalpha():
-            if len(list(guess)) != len(word):
-                if len(list(guess)) != 1:
+            if len(list_guess) != len(word):
+                if len(list_guess) != 1:
                     raise ValueError(
                         f'Your guess needs to contain 1 letter or\
                             {len(word)} letters'
                     )
-            if len(guess) == 1:
-                if guess[0] in hidden_word or guess[0] in tries:
+            if len(list_guess) == 1:
+                if guess in hidden_word or guess in tries:
                     raise ValueError(
                         f'You already guessed for {guess}'
                     )
-            if len(guess) == len(word):
-                if ''.join(guess) in tries:
+            if len(list_guess) == len(word):
+                if guess in tries:
                     raise ValueError(
                         f'You already guessed for {guess}'
                     )
@@ -185,8 +182,6 @@ def add_letter(guess, word, hidden_word, tries):
     Checks if there are any blanks left in the hidden word: If there are
     the game will continue, if not the game is won.
     """
-    guess = ''.join(guess)
-    print("Good guess! " + f'{guess.upper()} is in the word.')
     for i in range(len(word)):
         if word[i] == guess:
             hidden_word[i] = guess
@@ -194,7 +189,7 @@ def add_letter(guess, word, hidden_word, tries):
         get_guess(word, hidden_word, tries)
     else:
         print("\n")
-        print("The hidden word is: " + (' '.join(word)).upper() + "\n")
+        print("The hidden word is: " + (' '.join(word)) + "\n")
         end_game("win")
 
 
@@ -204,12 +199,12 @@ def collect_tries(tries, word, hidden_word, guess):
     7 objects wich is the total amount of tries the user has.
     If the list length is 7 the game is lost, otherwise the game continues.
     """
-    guess = ''.join(guess)
-    print("To bad! " + f'{guess.upper()} is not in the word')
-    tries.append(''.join(guess))
+    print("To bad! " + f'{guess} is not in the word')
+    tries.append(guess)
+    print(tries)
     if len(tries) == 7:
         print("\n")
-        print("The hidden word is: " + (' '.join(word)).upper() + "\n")
+        print("The hidden word is: " + (' '.join(word)) + "\n")
         end_game("loose")
     else:
         get_guess(word, hidden_word, tries)
@@ -252,6 +247,8 @@ def main():
     """
     tries = []
     word, hidden_word = get_word()
+    print(HANGMAN[len(tries)])
+    print(' '.join(hidden_word))
     get_guess(word, hidden_word, tries)
 
 
